fedb14fd0c6b1facd52fc389a2924d64f9bce40d
Nash Baek <nash4comp@gmail.com>
2022-03-03 20:21:09 -0800

    changed after login location: main.html to index.html

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -40,7 +40,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'main.html', // TODO: It should be connected after Dakaro finished main page.
+  signInSuccessUrl: 'index.html', // TODO: It should be connected after Dakaro finished main page.
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
