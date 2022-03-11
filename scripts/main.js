cebb27b8fcf4b65e3209d71289ae01fcd3f5e585
TodaysMenu <8leesw@gmail.com>
2022-03-11 00:51:37 -0800

    backend coding for comment/review page and resturant submission form to firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,66 @@
 function insertName() {
-  firebase.auth().onAuthStateChanged(user => {
-      // Check if user is signed in:
-      if (user) {                                                                 
-          // Do something for the current logged-in user here: 
-          console.log(user.uid);
-          //go to the correct user document by referencing to the user uid
-          currentUser = db.collection("users").doc(user.uid);
-          //get the document for current user.
-          currentUser.get()
-          .then(userDoc => {
-             var user_Name = userDoc.data().name;
-             console.log(user_Name);
-             //method #1:  insert with html only
-             //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-             //method #2:  insert using jquery
-             $("#name-goes-here").text(user_Name);                         //using jquery
-          })
-      } else {
-          // No user is signed in.
-      }
-  });
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    var user_Name = userDoc.data().name;
+                    console.log(user_Name);
+                    //method #1:  insert with html only
+                    //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+                    //method #2:  insert using jquery
+                    $("#name-goes-here").text(user_Name); //using jquery
+                })
+        } else {
+            // No user is signed in.
+        }
+    });
 }
 insertName();
 
-function populateCardsDynamically() {
-    let comboCardTemplate = document.getElementById("comboInfo");
-    let comboCardGroup = document.getElementById("comboCardGroup");
-    
-    db.collection("combos").get()
-        .then(allComobos => {
-            allCombos.forEach(doc => {
-                var hikeName = doc.data().actualPrice; //gets the name field
-
-                //start work here -sang
-
-                var hikeID = doc.data().id; //gets the unique ID field
-                var hikeLength = doc.data().length; //gets the length field
-                var hikeName = doc.data().name; //gets the name field
-                var hikeID = doc.data().id; //gets the unique ID field
-                var hikeLength = doc.data().length; //gets the length field
-                var hikeName = doc.data().name; //gets the name field
-                var hikeID = doc.data().id; //gets the unique ID field
-                var hikeLength = doc.data().length; //gets the length field
-                let testHikeCard = hikeCardTemplate.content.cloneNode(true);
-                testHikeCard.querySelector('.card-title').innerHTML = hikeName;
-                testHikeCard.querySelector('.card-length').innerHTML = hikeLength;
-                testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);
-                testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;
-                hikeCardGroup.appendChild(testHikeCard);
-            })
-
-        })
+function submitComment() {
+    console.log("in")
+    let Comment = document.getElementById("comment").value;
+    console.log(Comment);
+    db.collection("comments").add({
+        comment: Comment
+    })
 }
-populateCardsDynamically();
+
+
+
+    function populateCardsDynamically() {
+        let comboCardTemplate = document.getElementById("comboInfo");
+        let comboCardGroup = document.getElementById("comboCardGroup");
+
+        db.collection("combos").get()
+            .then(allComobos => {
+                allCombos.forEach(doc => {
+                    var hikeName = doc.data().actualPrice; //gets the name field
+
+                    //start work here -sang
+
+                    var hikeID = doc.data().id; //gets the unique ID field
+                    var hikeLength = doc.data().length; //gets the length field
+                    var hikeName = doc.data().name; //gets the name field
+                    var hikeID = doc.data().id; //gets the unique ID field
+                    var hikeLength = doc.data().length; //gets the length field
+                    var hikeName = doc.data().name; //gets the name field
+                    var hikeID = doc.data().id; //gets the unique ID field
+                    var hikeLength = doc.data().length; //gets the length field
+                    let testHikeCard = hikeCardTemplate.content.cloneNode(true);
+                    testHikeCard.querySelector('.card-title').innerHTML = hikeName;
+                    testHikeCard.querySelector('.card-length').innerHTML = hikeLength;
+                    testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);
+                    testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;
+                    hikeCardGroup.appendChild(testHikeCard);
+                })
+
+            })
+    }
+    populateCardsDynamically();
--- scripts/resSubmit.js
+++ scripts/resSubmit.js
@@ -1,14 +1,38 @@
-
 function submitComboInfo() {
     console.log("in")
     let Restaurant = document.getElementById("restName").value;
-    let Coupon = document.getEleementById("discountCode").value;
+    let DiscountCode = document.getEleementById("discountCode").value;
     let Name = document.getElementById("name").value;
     let RegularPrice = document.getElementById("actualPrice").value;
     let DiscountPrice = document.getElementById("discountedPrice").value;
-    let Item1 = document.getElementById("item1").value;
-    let Item2 = document.getElementById("item2").value;
-    let Item3 = document.getElementById("item3").value;
-    let Item4 = document.getElementById("item4").value;
+    let Details = document.getElementById("details").value;
     let Address = document.getElementById("restAddress").value;
-    console.log(Restaurant, Coupon, Name, RegularPrice, DiscountPrice, Item1, Item2, Item3, Item4);
+    let Website = document.getElementById("website").value;
+    let Telephone = document.getElementById("telephone").value;
+    let Cuisine = document.getElementById("cuisine").value;
+    console.log(Restaurant, Coupon, Name, RegularPrice, DiscountPrice, Details);
+
+    db.collection("restaurants").add({
+            //code: restID,
+            userID: userID,
+            name: Restaurant,
+            address: Address,
+            website: Website,
+            telephone: Telephone,
+
+            db.collection("combos").add({
+                name: Name,
+                details: Details,
+                cuisine: Cuisine,
+                restaurant: Restaurant,
+                discountCode: DiscountCode,
+                actualPrice: RegularPrice,
+                discountPrice: DiscountPrice,
+                discountRatio: RegularPrice / DiscountPrice
+                //add comments collection
+
+            })
+
+
+        }
+    })
