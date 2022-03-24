909347f611af64e6a817f926c1635bf7ccdbdb37
Dakaro Mueller <LukarioMC@gmail.com>
2022-03-24 11:05:47 -0700

    Fixed submit Javascript function

--- scripts/resSubmit.js
+++ scripts/resSubmit.js
@@ -1,7 +1,7 @@
 function submitComboInfo() {
-    console.log("in")
+    console.log("in");
     let Restaurant = document.getElementById("restName").value;
-    let DiscountCode = document.getEleementById("discountCode").value;
+    let DiscountCode = document.getElementById("discountCode").value;
     let Name = document.getElementById("name").value;
     let RegularPrice = document.getElementById("actualPrice").value;
     let DiscountPrice = document.getElementById("discountedPrice").value;
@@ -10,29 +10,22 @@ function submitComboInfo() {
     let Website = document.getElementById("website").value;
     let Telephone = document.getElementById("telephone").value;
     let Cuisine = document.getElementById("cuisine").value;
-    console.log(Restaurant, Coupon, Name, RegularPrice, DiscountPrice, Details);
+    console.log(Restaurant, DiscountCode, Name, RegularPrice, DiscountPrice, Details);
 
     db.collection("restaurants").add({
-            //code: restID,
-            userID: userID,
-            name: Restaurant,
-            address: Address,
-            website: Website,
-            telephone: Telephone,
+        name: Restaurant,
+        address: Address,
+        website: Website,
+        telephone: Telephone
+    });
 
-            db.collection("combos").add({
-                name: Name,
-                details: Details,
-                cuisine: Cuisine,
-                restaurant: Restaurant,
-                discountCode: DiscountCode,
-                actualPrice: RegularPrice,
-                discountPrice: DiscountPrice,
-                discountRatio: RegularPrice / DiscountPrice
-                //add comments collection
-
-            })
-
-
-        }
-    })
+    db.collection("combos").add({
+        details: Details,
+        cuisine: Cuisine,
+        restaurant: Restaurant,
+        discountCode: DiscountCode,
+        actualPrice: RegularPrice,
+        discountedPrice: DiscountPrice,
+        discountRatio: RegularPrice / DiscountPrice
+    });
+}
