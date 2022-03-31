8f38468a7ae3caeeaf486707c4d906b6d316aae7
Nash Baek <nash4comp@gmail.com>
2022-03-31 15:30:45 -0700

    Updated combo information

--- scripts/comboInfo.js
+++ scripts/comboInfo.js
@@ -1,35 +1,50 @@
 const queryString = window.location.search;
 let result = queryString.substring(4);
-console.log(result);
-// ?id=1oSoLcgg1pb3Qs5SH6PO
-
+let restaurant;
 
 function updateComboInfo() {
-  firebase.auth().onAuthStateChanged(user => {
-    // Check if user is signed in:
-    if (user) {
+  //go to the correct user document by referencing to the user uid
+  currentUser = db.collection("combos").doc(result)
+  //get the document for current user.
+  currentUser.get()
+    .then(userDoc => {
+      //get the data fields of the user
+      var details = userDoc.data().details;
+      var actualPrice = userDoc.data().actualPrice;
+      var discountedPrice = userDoc.data().discountedPrice;
+      var discountRatio = userDoc.data().discountRatio;
+      restaurant = userDoc.data().restaurant;
+      document.getElementById('comboName').innerHTML = details;
+      document.getElementById('originalPricePlaceholder').innerHTML = actualPrice;
+      document.getElementById('newPricePlaceholder').innerHTML = discountedPrice;
+      document.getElementById('differencePercentPlaceHolder').innerHTML = discountRatio * 100;
+      document.getElementById('restaurantInfoPlaceholder').innerHTML = restaurant;
+    })
 
-      //go to the correct user document by referencing to the user uid
-      currentUser = db.collection("combos").doc(result)
-      //get the document for current user.
-      currentUser.get()
-        .then(userDoc => {
-          //get the data fields of the user
-          var details = userDoc.data().details;
-          var actualPrice = userDoc.data().actualPrice;
-          var discountedPrice = userDoc.data().discountedPrice;
-          var discountRatio = userDoc.data().discountRatio;
-          document.getElementById('comboName').innerHTML = details;
-          document.getElementById('originalPricePlaceholder').innerHTML = actualPrice;
-          document.getElementById('newPricePlaceholder').innerHTML = discountedPrice;
-          document.getElementById('differencePercentPlaceHolder').innerHTML = discountRatio * 100;
-        })
-    } else {
-      // No user is signed in.
-      console.log("No user is signed in");
-    }
-  });
+  var delayInMilliseconds = 2000; //2 second
+
+  setTimeout(function () {
+    console.log("restaurant");
+    console.log(restaurant);
+    currentUser = db.collection("restaurants").doc(restaurant)
+    //get the document for current user.
+    currentUser.get()
+      .then(userDoc => {
+        //get the data fields of the user
+        var address = userDoc.data().address;
+        var telephone = userDoc.data().telephone;
+        var website = userDoc.data().website;
+        console.log(address);
+        console.log(telephone);
+        console.log(website);
+        document.getElementById('addressPlaceholder').innerHTML = address;
+        document.getElementById('telephonePlaceholder').innerHTML = telephone;
+        document.getElementById('websitePlaceholder').innerHTML = website;
+      })
+  }, delayInMilliseconds);
 }
 
-//call the function to run it 
 updateComboInfo();
+
+
+
