cef40c4542c5a51f026c6e0670aacfbb62535d39
Dakaro Mueller <LukarioMC@gmail.com>
2022-04-05 09:18:11 -0700

    Fixed restaurant information display, updated dollar amount display

--- scripts/comboInfo.js
+++ scripts/comboInfo.js
@@ -7,44 +7,45 @@ function updateComboInfo() {
   currentUser = db.collection("combos").doc(result)
   //get the document for current user.
   currentUser.get()
-    .then(userDoc => {
+    .then(comboDoc => {
+      let comboData = comboDoc.data();
       //get the data fields of the user
-      var details = userDoc.data().details;
-      var actualPrice = userDoc.data().actualPrice;
-      var discountedPrice = userDoc.data().discountedPrice;
-      var discountRatio = userDoc.data().discountRatio;
-      restaurant = userDoc.data().restaurant;
+      var details = comboData.details;
+      var actualPrice = parseFloat(comboData.actualPrice).toFixed(2);
+      var discountedPrice = parseFloat(comboData.discountedPrice).toFixed(2);
+      var discountRatio = parseFloat(comboData.discountRatio).toFixed(1);
+      restaurant = comboData.restaurant;
       document.getElementById('comboName').innerHTML = details;
       document.getElementById('originalPricePlaceholder').innerHTML = actualPrice;
       document.getElementById('newPricePlaceholder').innerHTML = discountedPrice;
       document.getElementById('differencePercentPlaceHolder').innerHTML = discountRatio * 100;
       document.getElementById('restaurantInfoPlaceholder').innerHTML = restaurant;
-    })
-
-  var delayInMilliseconds = 1000; //1 second delay
-
-  setTimeout(function () {
-    console.log("restaurant");
-    console.log(restaurant);
-    currentUser = db.collection("restaurants").doc(restaurant)
-    //get the document for current user.
-    currentUser.get()
-      .then(userDoc => {
-        //get the data fields of the user
-        var address = userDoc.data().address;
-        var telephone = userDoc.data().telephone;
-        var website = userDoc.data().website;
-        console.log(address);
-        console.log(telephone);
-        console.log(website);
-        document.getElementById('addressPlaceholder').innerHTML = address;
-        document.getElementById('telephonePlaceholder').innerHTML = telephone;
-        document.getElementById('websitePlaceholder').innerHTML = website;
-      })
-  }, delayInMilliseconds);
+      if(comboData.image != null) {
+        document.getElementById('comboImage').src = comboData.image;
+        console.log(comboData.image);
+      } else {
+        console.log(comboData.image);
+      }
+      
+      console.log("Restaurant:", restaurant);
+      currentRestaurant = db.collection("restaurants").doc(restaurant)
+      //get the document for current Restaurant.
+      currentRestaurant.get()
+        .then(resDoc => {
+          //get the data fields for the current restaurant
+          console.log(resDoc);
+          var resData = resDoc.data()
+          var address = resData.address;
+          var telephone = resData.telephone;
+          var website = resData.website;
+          
+          console.log(address, telephone, website);
+          
+          document.getElementById('addressPlaceholder').innerHTML = address;
+          document.getElementById('telephonePlaceholder').innerHTML = telephone;
+          document.getElementById('websitePlaceholder').innerHTML = website;
+      });
+    });
 }
 
 updateComboInfo();
-
-
-
